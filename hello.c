b97be40cfd4701c3313a3613d6ecd44b318146a6
Tej52 <tejsshiva52@gmail.com>
2024-01-11 18:14:32 +0530

    Errors fixed in printf function

--- hello.c
+++ hello.c
@@ -6,8 +6,8 @@ int main(){
     prinf("Enter X and Y Values to add two numers: ");
     scanf("%d%d", &x, &y);// i/p x and y values;
     z = x + y;
-    prinf("%d + %d = %d", x, y, z);
-    prinf("Hello world");
+    printf("%d + %d = %d", x, y, z);
+    printf("Hello world");
 
 
     return 0;
